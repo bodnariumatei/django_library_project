67de509d5291656d70ba8ad8018089d4469824ed
bodnariumatei <mateinicolae589@gmail.com>
2025-09-17 17:42:07 +0300

    library app refactorization

--- library/views.py
+++ library/views.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.shortcuts import render, redirect, get_object_or_404
 from library.models import Book, BorrowRecord
 from datetime import datetime as dt, timedelta
 
@@ -10,82 +9,59 @@ from datetime import datetime as dt, timedelta
 def landing(request):
     return render(request, "library/landing.html", {})
 
+def handle_approval(request):
+    if request.user.is_approved == False:
+        print("Your account is awaiting admin approval.")
+        return redirect("pending")
+
 @login_required
 def home(request):
-    if request.user.is_approved:
-        if request.method == "POST":
-            #print(request.POST)
-            if request.POST.get("borrow"):
-                book_id = request.POST.get("borrow")
-                b_book = Book.objects.get(id=book_id)
-                if b_book.no_available > 0:
-                    user = request.user
-                    b_book.no_available = b_book.no_available - 1
-                    b_book.save()
-                    borrow_book(user, b_book)
-                else:
-                    print("No more books available")
-                    #messages.info(request, message="We're sorry, there are no more copies of this book available.")
-            else:
-                print("Invalid input!")
+    handle_approval(request)
+    if request.method == "POST" and request.POST.get("borrow"):
+        handle_borrow_request(request, request.POST.get("borrow"))
+    books = Book.objects.all()
+    return render(request, "library/home.html", {"books": books})
 
-        books = Book.objects.all()
-        return render(request, "library/home.html", {"books":books})
-    else:
-        #messages.warning(request, "Your account is awaiting admin approval.")
-        return redirect("pending")
-
-
+@login_required
 def book(request, id):
-    if request.user.is_approved:
-        if request.method == "POST":
-            #print(request.POST)
-            if request.POST.get("borrow"):
-                book_id = request.POST.get("borrow")
-                b_book = Book.objects.get(id=book_id)
-                if b_book.no_available > 0:
-                    user = request.user
-                    b_book.no_available = b_book.no_available - 1
-                    b_book.save()
-                    borrow_book(user, b_book)
-                else:
-                    print("No more book available")
-                    #messages.info(request, message="We're sorry, there are no more copies of this book available.")
-            else:
-                print("Invalid input!")
-        book = Book.objects.get(id=id)
-        return render(request, "library/book.html", {"book":book})
-    else:
-        #messages.warning(request, "Your account is awaiting admin approval.")
-        return redirect("pending")
+    handle_approval(request)
+    if request.method == "POST" and request.POST.get("borrow"):
+        handle_borrow_request(request, request.POST.get("borrow"))
+    book_obj = get_object_or_404(Book, id=id)
+    return render(request, "library/book.html", {"book": book_obj})
 
+@login_required
 def borrowed(request):
-    if request.user.is_approved:
-        if request.POST.get("return"):
-            br_id = request.POST.get("return")
-            br = BorrowRecord.objects.get(id = br_id)
+    handle_approval(request)
+    if request.method == "POST" and request.POST.get("return"):
+        br_id = request.POST.get("return")
+        br = get_object_or_404(BorrowRecord, id=br_id)
+        if not br.returned:
             br.returned = True
             br.date_of_return = dt.now()
-            b_book = br.book
-            b_book.no_available += 1
-            b_book.save()
+            br.book.no_available += 1
+            br.book.save()
             br.save()
-        else:
-            print("Invalid input!")
+    brs = request.user.borrowrecord_set.all()
+    return render(request, "library/borrowed.html", {"brs": brs})
 
-        brs = request.user.borrowrecord_set.all()
-        return render(request, "library/borrowed.html", {"brs":brs})
-    else:
-        #messages.warning(request, "Your account is awaiting admin approval.")
-        return redirect("pending")
-
+@login_required
 def history(request):
-    if request.user.is_approved:
-        brs = request.user.borrowrecord_set.all()
-        return render(request, "library/history.html", {"brs":brs})
+    handle_approval(request)
+    brs = request.user.borrowrecord_set.all()
+    return render(request, "library/history.html", {"brs": brs})
+
+
+def handle_borrow_request(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    if book.no_available > 0:
+        book.no_available -= 1
+        book.save()
+        borrow_book(request.user, book)
+        return True
     else:
-        #messages.warning(request, "Your account is awaiting admin approval.")
-        return redirect("pending")
+        print(request, "We're sorry, there are no more copies of this book available.")
+        return False
 
 def borrow_book(user, b_book):
     br = BorrowRecord(user=user,
